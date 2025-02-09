de92e22d4e3cc5103a89ab40b3e43aef73b309c5
Joshriel Mive C. Carzon <joshymive@gmail.com>
2025-02-09 18:53:34 +0800

    Retrying push

--- app/src/main/java/com/aquamanagers/aquamanage_app/LoginActivity.kt
+++ app/src/main/java/com/aquamanagers/aquamanage_app/LoginActivity.kt
@@ -1,11 +1,58 @@
 package com.aquamanagers.aquamanage_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.aquamanagers.aquamanage_app.databinding.ActivityLoginBinding
+import com.google.firebase.Firebase
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        FirebaseApp.initializeApp(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = Firebase.auth
+
+        val currentUser = firebaseAuth.currentUser
+        if(currentUser!=null){
+            reload()
+        }
+
+        binding.signupLink.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.loginButton.setOnClickListener{
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+
+            if(email.isNotBlank() && password.isNotBlank()){
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        Toast.makeText(this, "Welcome back, $email", Toast.LENGTH_SHORT).show()
+                    } else{
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else{
+                Toast.makeText(this,"Fields cannot be left blank", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun reload() {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/aquamanagers/aquamanage_app/RegisterActivity.kt
+++ app/src/main/java/com/aquamanagers/aquamanage_app/RegisterActivity.kt
@@ -1,11 +1,57 @@
 package com.aquamanagers.aquamanage_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.aquamanagers.aquamanage_app.databinding.ActivityRegisterBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = Firebase.auth
+
+        binding.loginLink.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.signupButton.setOnClickListener{
+            val firstName = binding.firstname.text.toString()
+            val middleInitial = binding.middleInitial.text.toString()
+            val lastName = binding.lastname.text.toString()
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+            val confirmPassword = binding.confirmPassword.text.toString()
+
+            if(firstName.isNotBlank() && middleInitial.isNotBlank() && lastName.isNotBlank() && email.isNotBlank() && password.isNotBlank() && confirmPassword.isNotBlank()){
+                if(password == confirmPassword){
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
+                        if(it.isSuccessful){
+                            Toast.makeText(this, "Registration complete", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this, LoginActivity::class.java)
+                            startActivity(intent)
+                        } else{
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else{
+                    Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
+                }
+            } else{
+                Toast.makeText(this, "Fields cannot be left blank", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
