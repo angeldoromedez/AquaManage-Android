48bf4886c450b575edb665d3d0425636a4a4f7d4
Joshriel Mive C. Carzon <joshymive@gmail.com>
2025-02-09 23:19:14 +0800

    Few additions to UI for backend integration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     implementation(platform("com.google.firebase:firebase-bom:33.8.0"))
+    implementation("androidx.core:core-splashscreen:1.0.0")
 }
--- app/src/main/java/com/aquamanagers/aquamanage_app/LoginActivity.kt
+++ app/src/main/java/com/aquamanagers/aquamanage_app/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.aquamanagers.aquamanage_app.databinding.ActivityLoginBinding
 import com.google.firebase.Firebase
 import com.google.firebase.FirebaseApp
@@ -19,10 +20,12 @@ class LoginActivity : AppCompatActivity() {
         FirebaseApp.initializeApp(this)
         super.onCreate(savedInstanceState)
 
+        firebaseAuth = Firebase.auth
+        Thread.sleep(1500)
+        installSplashScreen()
+
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        firebaseAuth = Firebase.auth
 
         val currentUser = firebaseAuth.currentUser
         if(currentUser!=null){
@@ -42,6 +45,8 @@ class LoginActivity : AppCompatActivity() {
                 firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
                     if(it.isSuccessful){
                         Toast.makeText(this, "Welcome back, $email", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, DashboardActivity::class.java)
+                        startActivity(intent)
                     } else{
                         Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
                     }
@@ -52,7 +57,7 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun reload() {
-        TODO("Not yet implemented")
+    private fun reload(){
+        startActivity(Intent(this, DashboardActivity::class.java))
     }
 }
