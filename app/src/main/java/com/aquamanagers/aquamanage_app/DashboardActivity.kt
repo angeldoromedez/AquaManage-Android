4d5224b5e8684e950e3886185cff674f038e256a
Joshriel Mive C. Carzon <joshymive@gmail.com>
2025-02-09 23:38:42 +0800

    Added navigation to profile activity, please add UI

--- app/src/main/java/com/aquamanagers/aquamanage_app/DashboardActivity.kt
+++ app/src/main/java/com/aquamanagers/aquamanage_app/DashboardActivity.kt
@@ -1,11 +1,26 @@
 package com.aquamanagers.aquamanage_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aquamanagers.aquamanage_app.databinding.ActivityDashboardBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class DashboardActivity : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var binding: ActivityDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val currentUser = firebaseAuth.currentUser
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         setContentView(R.layout.activity_dashboard)
+
+        binding.profileIcon.setOnClickListener{
+            startActivity(Intent(this,ProfileActivity::class.java))
+        }
     }
 }
