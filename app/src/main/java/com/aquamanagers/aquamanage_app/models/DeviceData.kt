63581c5b06f964ae7f37640999217daa2e7e134c
Joshriel Mive C. Carzon <joshymive@gmail.com>
2025-05-07 20:25:17 +0800

    changed reminders symbols

--- app/src/main/java/com/aquamanagers/aquamanage_app/models/DeviceData.kt
+++ app/src/main/java/com/aquamanagers/aquamanage_app/models/DeviceData.kt
@@ -1,8 +1,30 @@
 package com.aquamanagers.aquamanage_app.models
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class DeviceData(
-    val controls: Int,
     val ph: Double,
     val tds: Double,
     val turbidity: Double
-)
+): Parcelable {
+    constructor(parcel: Parcel): this(
+        parcel.readDouble(),
+        parcel.readDouble(),
+        parcel.readDouble()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags:Int){
+        parcel.writeDouble(ph)
+        parcel.writeDouble(tds)
+        parcel.writeDouble(turbidity)
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR: Parcelable.Creator<DeviceItem> {
+        override fun createFromParcel(parcel: Parcel): DeviceItem = DeviceItem(parcel)
+
+        override fun newArray(size: Int): Array<DeviceItem?> = arrayOfNulls(size)
+    }
+}
